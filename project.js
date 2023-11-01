1c36d62075848ef65ee6d17772be6fc32499f2f4
LikhithaVakalapudi <vakalapudilikhitha0@users.noreply.github.com>
2023-11-01 20:17:23 +0530

    Added Responsiveness to the website

--- project.js
+++ project.js
@@ -56,17 +56,17 @@ const printProjects = () => {
         newproject.classList.add("new-project");
         newproject.innerHTML = 
         `<div class="project">
-        <div class="project-img">
-        <img src="${project.ProjectImg}" alt="${project.ImgAlt}" style="object-fit:cover; width:300px; height:350px;">
-        </div>
         <div class="project-details">
-            <h2>${project.projectName}</h2>
+            <h3>${project.projectName}</h3>
             <p><span>Tech Stack:</span> ${project.TechStack}</p>
             <p>${project.Description}</p>
             <div class="code-links">
                 <a href="${project.livePreview}" target="_blank" class="livepreview">Live Preview<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>open-in-new</title><path d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg></a>
                 <a href="${project.codeLink}" target= "_blank" class="codelink">View Code<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>xml</title><path d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" /></svg></a>
             </div>
+        </div>
+        <div class="project-img">
+        <img src="${project.ProjectImg}" alt="${project.ImgAlt}">
         </div>
     </div>`;
         projects.appendChild(newproject);
